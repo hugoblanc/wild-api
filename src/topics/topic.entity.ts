cd7edf40891c80efb5c048c89ff3c1862e7d1184
hugo <MrWhite>
2019-10-18 21:10:31 +0200

    feat:  create entity

--- src/topics/topic.entity.ts
+++ src/topics/topic.entity.ts
@@ -1,1 +1,30 @@
-export class Topic {}
+import { CreatedAt, UpdatedAt } from 'sequelize-typescript';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Topic {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 40 })
+  title: string;
+
+  @Column({ length: 150 })
+  description: string;
+
+  @Column({ length: 150 })
+  resourceUrl: string;
+
+//   npm install --save sequelize sequelize-typescript mysql2
+// npm install --save-dev @types/sequelize
+
+  @CreatedAt public createdAt: Date;
+
+  @UpdatedAt public updatedAt: Date;
+
+//   @DeletedAt public deletedAt: Date;
+
+// rattaché a un groupe ?
+// catégorie ou tag ?
+}
--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,1 +1,15 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class User {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ length: 30 })
+  firstname: string;
+
+  @Column({ length: 30 })
+  lastname: string;
+
+}
