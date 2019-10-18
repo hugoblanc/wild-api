652cdb8ac0e74895859f38e381928ab1d2d69580
hugo <MrWhite>
2019-10-18 20:56:59 +0200

    feat: integrate all route into api

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { FlashcardsModule } from './flashcards/flashcards.module';
+import { TicketsModule } from './tickets/tickets.module';
+import { TopicsModule } from './topics/topics.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, FlashcardsModule, TicketsModule, TopicsModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
