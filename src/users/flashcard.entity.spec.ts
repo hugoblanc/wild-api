23a96cb530835d4012cddbd5072257ce8c9021fd
hugo <MrWhite>
2019-10-18 20:56:24 +0200

    feat: add flashcard feature

---
+++ src/users/flashcard.entity.spec.ts
@@ -0,0 +1,7 @@
+import { Flashcard.Entity } from './flashcard.entity';
+
+describe('Flashcard.Entity', () => {
+  it('should be defined', () => {
+    expect(new Flashcard.Entity()).toBeDefined();
+  });
+});
