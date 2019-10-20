6a6ab20d6ba93ae5e693534370f77329da625ed2
Hugo <hugoblanc.blend@gmail.com>
2019-10-20 19:04:05 +0200

    feat:  many to many user <=> group

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,15 +1,20 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm';
+import { Group } from '../groups/group.entity';
 
 @Entity()
 export class User {
 
-  @PrimaryGeneratedColumn()
-  id: number;
+    @PrimaryGeneratedColumn()
+    id: number;
 
-  @Column({ length: 30 })
-  firstname: string;
+    @Column({ length: 30 })
+    firstname: string;
 
-  @Column({ length: 30 })
-  lastname: string;
+    @Column({ length: 30 })
+    lastname: string;
 
+    @ManyToMany(type => Group)
+    @JoinTable()
+    groups: Group[];
 }
+
