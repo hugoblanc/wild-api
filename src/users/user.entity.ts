314faec0c6716a2f7608eadb69760d6fbcf3021e
Hugo <hugoblanc.blend@gmail.com>
2019-10-20 19:18:04 +0200

    feat: description user/group entity

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,18 +1,23 @@
 import { Column, Entity, PrimaryGeneratedColumn, ManyToMany, JoinTable } from 'typeorm';
 import { Group } from '../groups/group.entity';
+import { ApiModelProperty } from '@nestjs/swagger';
 
 @Entity()
 export class User {
 
+    @ApiModelProperty()
     @PrimaryGeneratedColumn()
     id: number;
 
+    @ApiModelProperty()
     @Column({ length: 30 })
     firstname: string;
 
+    @ApiModelProperty()
     @Column({ length: 30 })
     lastname: string;
 
+    @ApiModelProperty({ type: [Group]})
     @ManyToMany(type => Group)
     @JoinTable()
     groups: Group[];
