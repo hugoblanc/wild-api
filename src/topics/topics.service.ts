327eaf05fc511dd5f4ef756cfb86841bf673932b
Hugo <hugoblanc.blend@gmail.com>
2019-10-20 18:37:56 +0200

    feat: route topic ok

--- src/topics/topics.service.ts
+++ src/topics/topics.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { Topic } from './topic.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { AbstractService } from '../core/abstract/abstract.service';
 
 @Injectable()
-export class TopicsService {}
+export class TopicsService extends AbstractService {
+
+    readonly logger = new Logger(TopicsService.name);
+    constructor(
+        @InjectRepository(Topic)
+        private readonly topicRepository: Repository<Topic>,
+    ) {
+        super(topicRepository);
+    }
+}
