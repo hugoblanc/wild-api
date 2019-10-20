970931a2a939cf331944d8de0da197830362b4c6
Hugo <hugoblanc.blend@gmail.com>
2019-10-20 18:40:15 +0200

    chore: structural change

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,31 +7,31 @@ import { FlashcardsModule } from './flashcards/flashcards.module';
 import { TicketsModule } from './tickets/tickets.module';
 import { TopicsModule } from './topics/topics.module';
 import { EventsModule } from './events/events.module';
-import { CoreModule } from './core/core.module';
+import { GroupsModule } from './groups/groups.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.WILD_DB_HOST,
-      port: parseInt(process.env.WILD_DB_PORT, 10),
-      username: process.env.WILD_DB_USER,
-      password: process.env.WILD_DB_PASSWORD,
-      database: 'wild',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
-      extra: {
-        charset: 'utf8mb4_general_ci',
-      },
-    }),
-    UsersModule,
-    FlashcardsModule,
-    TicketsModule,
-    TopicsModule,
-    EventsModule,
-    CoreModule,
-  ],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'mysql',
+            host: process.env.WILD_DB_HOST,
+            port: parseInt(process.env.WILD_DB_PORT, 10),
+            username: process.env.WILD_DB_USER,
+            password: process.env.WILD_DB_PASSWORD,
+            database: 'wild',
+            entities: [__dirname + '/**/*.entity{.ts,.js}'],
+            synchronize: true,
+            extra: {
+                charset: 'utf8mb4_general_ci',
+            },
+        }),
+        UsersModule,
+        FlashcardsModule,
+        TicketsModule,
+        TopicsModule,
+        EventsModule,
+        GroupsModule,
+    ],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule { }
