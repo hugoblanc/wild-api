e991ac40df87f908e7a5b5c847ea7b6eee584a7b
Hugo <hugoblanc.blend@gmail.com>
2019-10-20 18:38:09 +0200

    feat: route tickets

--- src/tickets/tickets.service.ts
+++ src/tickets/tickets.service.ts
@@ -1,4 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { AbstractService } from '../core/abstract/abstract.service';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Ticket } from './ticket.entity';
 
 @Injectable()
-export class TicketsService {}
+export class TicketsService extends AbstractService {
+
+    readonly logger = new Logger(TicketsService.name);
+
+    constructor(
+        @InjectRepository(Ticket)
+        private readonly ticketRepository: Repository<Ticket>,
+    ) {
+        super(ticketRepository);
+    }
+
+}
