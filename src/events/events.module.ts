02ac8ea7f12bb0053284875c5fd90cb2719ef8ce
Hugo <hugoblanc.blend@gmail.com>
2019-10-20 16:53:38 +0200

    feat: add entity repository

--- src/events/events.module.ts
+++ src/events/events.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { EventsService } from './events.service';
 import { EventsController } from './events.controller';
+import { Event } from './event.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  providers: [EventsService],
-  controllers: [EventsController]
+    imports: [TypeOrmModule.forFeature([Event])],
+    providers: [EventsService],
+    controllers: [EventsController]
 })
-export class EventsModule {}
+export class EventsModule { }
--- src/tickets/tickets.module.ts
+++ src/tickets/tickets.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TicketsService } from './tickets.service';
 import { TicketsController } from './tickets.controller';
+import { Ticket } from './ticket.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  providers: [TicketsService],
-  controllers: [TicketsController]
+    imports: [TypeOrmModule.forFeature([Ticket])],
+    providers: [TicketsService],
+    controllers: [TicketsController]
 })
-export class TicketsModule {}
+export class TicketsModule { }
