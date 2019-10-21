e6e8f73ef986b1cefa56d58c3f8bb37d6413df28
MrWhite <hugoblanc.blend@gmail.com>
2019-10-21 19:28:45 +0200

    feat: event validation

--- src/events/event.entity.ts
+++ src/events/event.entity.ts
@@ -3,6 +3,7 @@ import { Column, Entity, ManyToOne } from 'typeorm';
 import { AbstractEntity } from '../core/abstract/abstract.entity';
 import { ApiModelProperty } from '@nestjs/swagger';
 import { User } from '../users/user.entity';
+import { IsNotEmpty } from 'class-validator';
 
 @Entity()
 export class Event extends AbstractEntity {
@@ -13,10 +14,12 @@ export class Event extends AbstractEntity {
 
     @ApiModelProperty({ type: 'string', format: 'date-time', example: '2018-11-21T06:20:32.232Z' })
     @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    @IsNotEmpty()
     startAt: Date;
 
     @ApiModelProperty({ type: 'string', format: 'date-time', example: '2018-11-21T06:20:32.232Z' })
     @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    @IsNotEmpty()
     endAt: Date;
 
     @ApiModelProperty({ type: User})
