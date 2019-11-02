050b7fff80329af7231e25c680a50baf17374dac
MrWhite <hugoblanc.blend@gmail.com>
2019-11-02 15:13:16 +0100

    feat: add status to ticket

--- src/tickets/ticket.entity.ts
+++ src/tickets/ticket.entity.ts
@@ -1,9 +1,10 @@
 import { Group } from './../groups/group.entity';
-import { Entity, ManyToOne } from 'typeorm';
+import { Entity, ManyToOne, Column } from 'typeorm';
 import { AbstractEntity } from '../core/abstract/abstract.entity';
 import { User } from '../users/user.entity';
 import { ApiModelProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
+import { TicketStatus } from './ticket-status.enum';
 
 @Entity()
 export class Ticket extends AbstractEntity {
@@ -17,4 +18,7 @@ export class Ticket extends AbstractEntity {
     @ManyToOne(type => Group, group => group.tickets, { cascade: ['insert', 'update'] })
     @IsNotEmpty()
     group: Group;
+
+    @Column({ type: 'enum', enum: TicketStatus, default: TicketStatus.ASKED })
+    status: TicketStatus;
 }
