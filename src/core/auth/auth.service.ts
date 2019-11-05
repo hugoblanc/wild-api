8e675ba703082cbd3269dcb33ec4b1a475be4226
MrWhite <hugoblanc.blend@gmail.com>
2019-11-05 14:51:17 +0100

    fix: prod

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -75,7 +75,7 @@ export class AuthService {
     private createOauthConf(code: string) {
         const credentials = {
             code,
-            redirect_uri: 'http://localhost:3000/auth/oauth',
+            redirect_uri: 'https://wild-api.witpoc.com/auth/oauth',
             grant_type: 'authorization_code',
             client_secret: '44f6416225beb466428c70c92e0a7a50c42c88573f2f99784b24095ba446c2dd',
             client_id: '0dd2e3ce72b4e0345c8f41c23b4e1da2a9e81f95d6a50b14e2c0053a95b47cc1',
