31b6077757e69e454ed8eb4892eaa31ed49a6e4d
MrWhite <hugoblanc.blend@gmail.com>
2019-11-05 15:56:45 +0100

    working redirection

--- src/core/auth/auth.controller.ts
+++ src/core/auth/auth.controller.ts
@@ -9,13 +9,19 @@ export class AuthController {
     }
 
     @Get('oauth')
-    setupOAuth(@Query('code') query) {
-        return this.service.handleOAuthCallback(query);
+    @Redirect('http://localhost:4200/')
+    async setupOAuth(@Query('code') query) {
+
+        const token = await this.service.handleOAuthCallback(query).toPromise();
+        console.log('http://localhost:4200/auth/' + token.access_token);
+
+        return { url: 'http://localhost:4200/auth/' + token.access_token };
     }
 
     @Get()
-    @Redirect(AuthService.OAUTH_HOST + '/authorize?client_id=0dd2e3ce72b4e0345c8f41c23b4e1da2a9e81f95d6a50b14e2c0053a95b47cc1&response_type=code&redirect_uri=https%3A%2F%2Fwild-api.witpoc.com%2Fauth%2Foauth', 302)
+    @Redirect(AuthService.OAUTH_HOST + 'authorize?client_id=0dd2e3ce72b4e0345c8f41c23b4e1da2a9e81f95d6a50b14e2c0053a95b47cc1&response_type=code&redirect_uri=https%3A%2F%2Fwild-api.witpoc.com%2Fauth%2Foauth', 302)
     getAuth() {
+
         return { url: AuthService.OAUTH_HOST + 'authorize?client_id=0dd2e3ce72b4e0345c8f41c23b4e1da2a9e81f95d6a50b14e2c0053a95b47cc1&response_type=code&redirect_uri=https%3A%2F%2Fwild-api.witpoc.com%2Fauth%2Foauth' };
     }
 
--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,7 +1,7 @@
 import { OauthResponse } from './oauth-response';
 import { Injectable, HttpService } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { map, flatMap } from 'rxjs/operators';
+import { map, flatMap, catchError } from 'rxjs/operators';
 import querystring = require('querystring');
 import { AxiosRequestConfig } from 'axios';
 import { OdysseyService } from '../../odyssey/odyssey.service';
@@ -32,7 +32,11 @@ export class AuthService {
         const odysseyDTO$ = oauth$.pipe(flatMap((odyResponse: OauthResponse) => {
             const axiosConfig = this.createBearerConfig(odyResponse.accessToken);
             return this.odysseyService.getCurrentUser(axiosConfig);
-        }));
+        }),
+            catchError((error) => {
+                throw error;
+            },
+        ));
 
         // Save ou update du user
         const user$ = odysseyDTO$.pipe(
@@ -68,7 +72,7 @@ export class AuthService {
 
     login(user: any) {
         return {
-            access_token: this.jwtService.sign({payload: user}),
+            access_token: this.jwtService.sign({ payload: user }),
         };
     }
 
