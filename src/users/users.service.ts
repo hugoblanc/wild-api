13f5453125433ac857aed97b82333b8108d2da72
MrWhite <hugoblanc.blend@gmail.com>
2019-11-06 14:29:29 +0100

    fix: api

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,10 +1,8 @@
-import { Injectable, Logger, Scope, Inject } from '@nestjs/common';
+import { Injectable, Logger, Scope } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TypeOrmCrudService } from '@nestjsx/crud-typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
-import { REQUEST } from '@nestjs/core';
-import { Request } from 'express';
 
 @Injectable({ scope: Scope.REQUEST })
 export class UsersService extends TypeOrmCrudService<User> {
@@ -13,7 +11,6 @@ export class UsersService extends TypeOrmCrudService<User> {
     constructor(
         @InjectRepository(User)
         private readonly repository: Repository<User>,
-        @Inject(REQUEST) private readonly request: any,
     ) {
         super(repository);
     }
@@ -22,7 +19,4 @@ export class UsersService extends TypeOrmCrudService<User> {
         return this.repository.save(content);
     }
 
-    findMe() {
-        return this.request.user;
-    }
 }
